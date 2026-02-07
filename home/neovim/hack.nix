9402deff4af17538af52155024416724b35eec4c
Craig Furman <craig@craigfurman.dev>
2026-02-07 12:30:58 +0000

    nvim: allow project-scoped config
    Control it with an env var to avoid getting hosed by malicious config I
    haven't read yet.

--- home/neovim/hack.nix
+++ home/neovim/hack.nix
@@ -2,5 +2,12 @@
 {
   # This still comes before plugin config. I'm not sure why. Might have to fix
   # that one day.
-  programs.neovim.extraLuaConfig = lib.mkAfter "require('hack')";
+  programs.neovim.extraLuaConfig = lib.mkAfter ''
+    local extraConfig = os.getenv("NVIM_EXTRA_CONFIG")
+    if extraConfig ~= nil then
+      dofile(extraConfig)
+    end
+
+    require('hack');
+  '';
 }
