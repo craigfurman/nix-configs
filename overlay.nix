399c038c6c2d114633bb13213e58acbba313f5b0
Craig Furman <craig@craigfurman.dev>
2026-03-27 07:56:34 +0000

    Bump HM

--- overlay.nix
+++ overlay.nix
@@ -1,7 +1,2 @@
 final: prev: {
-  vimPlugins = prev.vimPlugins // {
-    telescope-nvim = prev.vimPlugins.telescope-nvim.overrideAttrs (prevAttrs: {
-      dependencies = (prevAttrs.dependencies or [ ]) ++ [ final.vimPlugins.plenary-nvim ];
-    });
-  };
 }
